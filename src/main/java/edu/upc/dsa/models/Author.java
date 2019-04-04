162a3f84a7ba03dc4b39d9b0bbf464f02992cee7
Izan Pérez Cosano <izanperez98@gmail.com>
2019-04-04 18:37:40 +0200

    added id attribute

--- src/main/java/edu/upc/dsa/models/Author.java
+++ src/main/java/edu/upc/dsa/models/Author.java
@@ -1,20 +1,24 @@
 package edu.upc.dsa.models;
 
+import edu.upc.dsa.util.RandomUtils;
+
 import java.util.LinkedList;
 
 public class Author {
 
     //Attributes
+    private String id;
     private String name;
     private LinkedList<Track> tracks;
     private LinkedList<Album> albums;
 
     //Constructors
     public Author() {
-
+        this.id = RandomUtils.getId();
     }
 
     public Author(String name) {
+        this();
         this.name = name;
         this.tracks = new LinkedList<>();
         this.albums = new LinkedList<>();
@@ -30,6 +34,14 @@ public class Author {
     }
 
     //Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
